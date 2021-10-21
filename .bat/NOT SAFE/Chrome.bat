7d2666c6778a4ba038355b58abb0c66cd8595bc2
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2021-10-21 10:54:43 -0500

    Create Chrome.bat

---
+++ .bat/NOT SAFE/Chrome.bat
@@ -0,0 +1,65 @@
+@echo off 
+
+echo if you downloaded this take out Timeout /t 30 and this line of code but, dont delete @echo off
+echo I added this just in case u clicked this by mistake but, to test this code download chrome if you dont have it and press any key 3 times ok bye
+
+Timeout /T 30
+echo get ready 
+Timeout /T 1
+echo 3
+Timeout /T 1
+echo 2
+Timeout /T 1
+echo 1
+Timeout /T 1
+echo Go
+goto Crash
+pause
+
+:Crash
+clr
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+start chrome 
+start chrome
+start chrome
+goto Crash
+pause
